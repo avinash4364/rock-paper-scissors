685cae1afa748b5eec23f5d7eb429e7c88e4ac25
aky <aky25199@gmail.com>
2025-12-06 14:01:39 +0530

    Modify and Comment
    Made the necessary comments for future reference and Check undefined user input to avoid errors

--- script.js
+++ script.js
@@ -1,158 +1,98 @@
-// let humanScore = 0;
-// let computerScore = 0;
+// This is main function which lets user play rock-paper-scissors against computer 5 times.
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-// function playGame() {
-//     // increment score for whoever wins amongst computer and human
-
-//     function playRound(humanChoice, computerChoice) {
-//         let hc = humanChoice.toLowerCase();
-//         let humanWin;
-//         let computerWin;
-//         if (hc === "rock") {
-//             if (computerChoice === "paper") {
-//                 computerScore++;
-//                 computerWin = true;
-//             } else if (computerChoice === "scissors") {
-//                 humanScore++;
-//                 humanWin = true;
-//             }
-//         } else if (hc === "paper") {
-//             if (computerChoice === "rock") {
-//                 humanWin = true;
-//                 humanScore++;
-//             } else if (computerChoice === "scissors") {
-//                 computerScore++;
-//                 computerWin = true;
-//             }
-//         } else {
-//             if (computerChoice === "paper") {
-//                 humanWin = true;
-//                 humanScore++;
-//             } else if (computerChoice === "rock") {
-//                 computerScore++;
-//                 computerWin = true;
-//             }
-//         }
-
-//         if (humanWin) {
-//             console.log(`You rock! ${humanChoice} beats ${computerChoice}`);
-//             console.log(humanScore);
-//         } else if (computerWin) {
-//             console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-//             console.log(computerScore);
-//         } else {
-//             console.log(`oops! It's a draw`);
-//         }
-//     }
-
-//     // get the computer choice b/w rock , paper , scissors
-//     function getComputerChoice() {
-//         let choice;
-//         const randomNum = Math.floor(Math.random() * 3 + 1);
-//         //   console.log(randomNum);
-//         switch (randomNum) {
-//             case 1:
-//                 choice = "rock";
-//                 break;
-//             case 2:
-//                 choice = "paper";
-//                 break;
-//             case 3:
-//                 choice = "scissors";
-//                 break;
-//             default:
-//                 break;
-//         }
-//         return choice;
-//     }
-
-//     // get the input from the user b/w rock, paper and scissors
-//     function getHumanChoice() {
-//         const choice = prompt("Choose b/w rock, paper and scissors");
-//         return choice;
-//     }
-
-//     // call the playround function 5 times
-//     for (let index = 0; index < 5; index++) {
-//         let humanSelection = getHumanChoice();
-//         let computerSelection = getComputerChoice();
-//         playGame(humanSelection, computerSelection);
-//     }
-// }
-
-// playGame();
-
-let humanScore = 0;
-let computerScore = 0;
-
-function getHumanChoice() {
-    let choice = prompt("Choose b/w rock, paper and scissors").toLowerCase();
-    const choiceArray = ["rock", "paper", "scissors"];
-    if (choiceArray.includes(choice)) return choice;
-    else {
-        choice = prompt("You Chose wrong, Try again").toLowerCase();
-        if (choiceArray.includes(choice)) return choice;
+    // function to ask user to choose b/w rock , paper and scissors
+    // a user can enter his option in capital or small letters
+    function getHumanChoice() {
+        let choice = prompt(
+            "Choose b/w rock, paper and scissors"
+        ).toLowerCase();
+        const choiceArray = ["rock", "paper", "scissors"];
+        if (choice && choiceArray.includes(choice))
+            return choice; // here it is also checked for undefined values
+        // if he choses any other options than the ones in the choiceArray
+        // he is given one more chance to make the correct choice again, otherwise he loses to computer by default
         else {
-            alert("Well, You chose wrong again, computer wins by default");
-            return "";
-        }
-    }
-}
-
-function getComputerChoice() {
-    const choiceArray = ["rock", "paper", "scissors"];
-    const random = Math.floor(Math.random() * 3);
-    return choiceArray[random];
-}
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice) {
-        console.log(
-            `human chose ${humanChoice} and computer chose ${computerChoice}`
-        );
-        if (humanChoice == computerChoice) {
-            console.log("It's a draw");
-        } else {
-            if (humanChoice == "rock") {
-                if (computerChoice == "paper") computerScore++;
-                else humanScore++;
-            } else if (humanChoice == "paper") {
-                if (computerChoice == "scissors") computerScore++;
-                else humanScore++;
-            } else {
-                // here the choice will default to scissors for the human
-                if (computerChoice == "rock") computerScore++;
-                else humanScore++;
+            choice = prompt("You Chose wrong, Try again").toLowerCase();
+            if (choice && choiceArray.includes(choice)) return choice;
+            else {
+                alert("Well, You chose wrong again, computer wins by default");
+                return ""; // return an empty string if the user choses nothing or any value other than rock, paper and scissors
             }
         }
-    } else {
-        console.log(
-            "human chose the wrong option and computer wins by default"
-        );
-        computerScore++;
     }
+
+    // get computer choice b/w rock , paper and scissors using Math.random() function of JavaScript
+    function getComputerChoice() {
+        const choiceArray = ["rock", "paper", "scissors"];
+        const random = Math.floor(Math.random() * 3);
+        return choiceArray[random];
+    }
+
+    // comparing the human choice and computer choice and incrementing the score of the winner
+    // the score of both human and computer is a top local variable so that it is accessible everywhere in the current function.
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice) {
+            // if human chose amongst rock, paper and scissors
+            console.log(
+                `human chose ${humanChoice} and computer chose ${computerChoice}`
+            );
+            if (humanChoice == computerChoice) {
+                console.log("It's a draw");
+            } else {
+                if (humanChoice == "rock") {
+                    if (computerChoice == "paper") computerScore++;
+                    else humanScore++;
+                } else if (humanChoice == "paper") {
+                    if (computerChoice == "scissors") computerScore++;
+                    else humanScore++;
+                } else {
+                    // here the choice will default to scissors for the human
+                    if (computerChoice == "rock") computerScore++;
+                    else humanScore++;
+                }
+            }
+        } else {
+            // if the human chose the wrong option (which could be null, undefined or other values)
+            console.log(
+                "human chose the wrong option and computer wins by default"
+            );
+            computerScore++;
+        }
+    }
+
+    // function which lets user and computer play n rounds of rock-paper-scissors with each other
+    // by default they will play with each other 5 times - done using default parameter
+    function playNRounds(rounds = 5) {
+        let humanChoice;
+        let computerChoice;
+        for (let round = 0; round < rounds; round++) {
+            humanChoice = getHumanChoice();
+            computerChoice = getComputerChoice();
+            playRound(humanChoice, computerChoice);
+        }
+        console.group("The final Score:");
+        console.log(`human: ${humanScore}`);
+        console.log(`computer: ${computerScore}`);
+        console.groupEnd();
+
+        if (humanScore === computerScore) {
+            console.log("%cIt's a draw😞😞", "font-size:24px");
+        } else {
+            if (humanScore > computerScore) {
+                console.log(
+                    "%cYou Win😀🥳",
+                    "font-size:36px; font-weight:bold;"
+                ); // the %c specifier acts as a placeholder for where the CSS styling should begin and the second argument can be used to specify the CSS styling to apply
+            } else console.log("%cYou Lost🥹😕", "font-size:12px");
+        }
+    }
+
+    // play game b/w computer and user n times (by default it is 5 times)
+    playNRounds();
 }
 
-function playGame() {
-    let humanChoice;
-    let computerChoice;
-    for (let i = 0; i < 5; i++) {
-        humanChoice = getHumanChoice();
-        computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
-    console.group("The final Score:");
-    console.log(`human: ${humanScore}`);
-    console.log(`computer: ${computerScore}`);
-    console.groupEnd();
-
-    if (humanScore === computerScore) {
-        console.log("It's a draw");
-    } else {
-        if (humanScore > computerScore) {
-            console.log("You Win!");
-        } else console.log();
-    }
-}
-
+// play rock-paper-scissors b/w user and computer
 playGame();
