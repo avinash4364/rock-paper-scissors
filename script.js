ed9b1ec71f480343bbdc9e2b88be874145e95c12
aky <aky25199@gmail.com>
2025-12-11 20:09:57 +0530

    Add data attribute in html for hand signs for cleaner logic in our JS

--- script.js
+++ script.js
@@ -1,85 +1,67 @@
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-    function getHumanChoice() {
-        let choice = prompt("Choose b/w rock, paper and scissors")
-            .toLowerCase()
-            .trim();
-        const choiceArray = ["rock", "paper", "scissors"];
-        if (choice && choiceArray.includes(choice))
-            // checks for undefined values
-            return choice;
+function getHumanChoice() {
+    let choice = prompt("Choose b/w rock, paper and scissors")
+        .toLowerCase()
+        .trim();
+    const choiceArray = ["rock", "paper", "scissors"];
+    if (choice && choiceArray.includes(choice))
+        // checks for undefined values
+        return choice;
+    else {
+        choice = prompt("You Chose wrong, Try again").toLowerCase().trim();
+        if (choice && choiceArray.includes(choice)) return choice;
         else {
-            choice = prompt("You Chose wrong, Try again").toLowerCase().trim();
-            if (choice && choiceArray.includes(choice)) return choice;
-            else {
-                alert("Well, You chose wrong again, computer wins by default");
-                return "";
-            }
+            alert("Well, You chose wrong again, computer wins by default");
+            return "";
         }
     }
-
-    function getComputerChoice() {
-        const choiceArray = ["rock", "paper", "scissors"];
-        const random = Math.floor(Math.random() * 3);
-        return choiceArray[random];
-    }
-
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice) {
-            console.log(
-                `human chose ${humanChoice} and computer chose ${computerChoice}`
-            );
-            if (humanChoice == computerChoice) {
-                console.log("It's a draw");
-            } else {
-                if (humanChoice == "rock") {
-                    if (computerChoice == "paper") computerScore++;
-                    else humanScore++;
-                } else if (humanChoice == "paper") {
-                    if (computerChoice == "scissors") computerScore++;
-                    else humanScore++;
-                } else {
-                    // here the choice will default to scissors for the human
-                    if (computerChoice == "rock") computerScore++;
-                    else humanScore++;
-                }
-            }
-        } else {
-            console.log(
-                "human chose the wrong option and computer wins by default"
-            );
-            computerScore++;
-        }
-    }
-
-    function playNRounds(rounds = 5) {
-        let humanChoice;
-        let computerChoice;
-        for (let round = 0; round < rounds; round++) {
-            humanChoice = getHumanChoice();
-            computerChoice = getComputerChoice();
-            playRound(humanChoice, computerChoice);
-        }
-        console.group("The final Score:");
-        console.log(`human: ${humanScore}`);
-        console.log(`computer: ${computerScore}`);
-        console.groupEnd();
-
-        if (humanScore === computerScore) {
-            console.log("%cIt's a draw😞😞", "font-size:24px");
-        } else {
-            if (humanScore > computerScore) {
-                console.log(
-                    "%cYou Win😀🥳",
-                    "font-size:36px; font-weight:bold;"
-                ); // the %c specifier acts as a placeholder for where the CSS styling should begin and the second argument can be used to specify the CSS styling to apply
-            } else console.log("%cYou Lost🥹😕", "font-size:18px");
-        }
-    }
-
-    playNRounds();
 }
 
-playGame();
+function getComputerChoice() {
+    const choiceArray = ["rock", "paper", "scissors"];
+    const random = Math.floor(Math.random() * 3);
+    return choiceArray[random];
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice) {
+        console.log(
+            `human chose ${humanChoice} and computer chose ${computerChoice}`
+        );
+        if (humanChoice == computerChoice) {
+            console.log("It's a draw");
+        } else {
+            if (humanChoice == "rock") {
+                if (computerChoice == "paper") computerScore++;
+                else humanScore++;
+            } else if (humanChoice == "paper") {
+                if (computerChoice == "scissors") computerScore++;
+                else humanScore++;
+            } else {
+                // here the choice will default to scissors for the human
+                if (computerChoice == "rock") computerScore++;
+                else humanScore++;
+            }
+        }
+    } else {
+        console.log(
+            "human chose the wrong option and computer wins by default"
+        );
+        computerScore++;
+    }
+}
+
+const overlayBtn = document.querySelector(".overlay-btn");
+const handSigns = document.querySelectorAll(".buttons img");
+function removeOverlay() {
+    overlayBtn.parentElement.classList.remove("game-start");
+}
+
+handSigns.forEach((handSign) =>
+    handSign.addEventListener("click", function (e) {
+        console.log(e.target.getAttribute("data-value"));
+    })
+);
+overlayBtn.addEventListener("click", removeOverlay);
